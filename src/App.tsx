05dcb4076dc7773611aa8eac08ea00a778c66143
bosiofinance <bosiofinance@gmail.com>
2025-07-20 14:49:49 -0300

    Update App.tsx
    gen 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import Funcionalidades from "./pages/Funcionalidades";
+import Sobre from "./pages/Sobre";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
